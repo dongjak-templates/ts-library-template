6f8d4f76472f512b3ebae5f2f22e405c3ce93319
cruldra <cruldra@gmail.com>
2023-11-25 17:18:28 +0800

    11

---
+++ template-cleanup.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const fs = require('fs');
+const Handlebars = require('handlebars');
+
+// 获取当前命令行上下文路径
+const currentDirectory = process.cwd();
+// 获取 package.json 文件中的内容
+const packageJsonPath = path.join(currentDirectory, 'package.json');
+const packageJsonFile = fs.readFileSync(packageJsonPath, 'utf8');
+const viteConfigPath = path.join(currentDirectory, 'vite.config.ts');
+const viteConfigFile =  fs.readFileSync(viteConfigPath, 'utf8');
+const packageJsonTemplate = Handlebars.compile(packageJsonFile);
+const viteConfigTemplate = Handlebars.compile(viteConfigFile);
+const data = {
+    repo_name: process.argv[2],
+    author: process.argv[3],
+    repo_url    : process.argv[4],
+
+
+};
+const output = packageJsonTemplate(data);
+const output2 = viteConfigTemplate(data);
+fs.writeFileSync(packageJsonPath, output);
+fs.writeFileSync(viteConfigPath, output2);
